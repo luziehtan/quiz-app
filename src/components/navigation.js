b212936f0c43d4340214451b9a1cbcaf32d0760f
Luciana Blümlein <luziehtan@hotmail.com>
2021-02-01 17:36:09 +0100

    Update js navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,41 +1,22 @@
-import getByDataJs from '../js/getByDataJs'
+import getAllByDataJs from '../js/getAllByDataJs'
 
 export default function navigation() {
-  const index = getByDataJs('index')
-  const bookmark = getByDataJs('bookmark')
-  const createNew = getByDataJs('createNew')
+  const pages = getAllByDataJs('page')
+  const navButtons = getAllByDataJs('nav')
 
-  const iconHome = getByDataJs('iconHome')
-  const iconBookmark = getByDataJs('iconBookmark')
-  const iconCreate = getByDataJs('iconCreate')
-
-  iconHome.addEventListener('click', () => {
-    index.hidden = false
-    bookmark.hidden = true
-    createNew.hidden = true
-
-    iconHome.classList.add('active')
-    iconBookmark.classList.remove('active')
-    iconCreate.classList.remove('active')
-  })
-
-  iconBookmark.addEventListener('click', () => {
-    index.hidden = true
-    bookmark.hidden = false
-    createNew.hidden = true
-
-    iconHome.classList.remove('active')
-    iconBookmark.classList.add('active')
-    iconCreate.classList.remove('active')
-  })
-
-  iconCreate.addEventListener('click', () => {
-    index.hidden = true
-    bookmark.hidden = true
-    createNew.hidden = false
-
-    iconHome.classList.remove('active')
-    iconBookmark.classList.remove('active')
-    iconCreate.classList.add('active')
+  navButtons.forEach(button => {
+    const clickedButtonName = button.dataset.name
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.name
+        page.classList.toggle('hidden', clickedButtonName !== pageName)
+      })
+      navButtons.forEach(button => {
+        button.classList.toggle(
+          'active',
+          clickedButtonName === button.dataset.name
+        )
+      })
+    })
   })
 }
