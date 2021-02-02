f71c6baea83bb953eaf839b1c8416ae1257e0e92
Luciana Blümlein <luziehtan@hotmail.com>
2021-02-02 18:04:01 +0100

    Add random question button

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,23 @@ navigation()
 bookmark()
 formular()
 quizCardAnswer()
+
+// IDEA: randomizer button for random question
+
+const quizCard = document.querySelector('[data-js=quiz-card]')
+const buttonRandom = document.querySelector('[data-js=randomButton]')
+const number = quizCard.length
+
+buttonRandom.addEventListener('click', () => {
+  const questions = document.querySelectorAll('[data-js="quiz-card"]')
+  const randomIndex = getRandom(number - 1)
+  questions.forEach((question, index) => {
+    const isCardSelected = index === randomIndex
+    question.classList.toggle('selected', isCardSelected)
+    console.log('button clicked')
+  })
+})
+
+function getRandom(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
